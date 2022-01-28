69de03e2ec82c967294a467dae58e1b5847460d4
LaurenDarrimon <laurendarrimon@gmail.com>
2022-01-28 15:55:56 -0800

    change port to process.env or local host for heroku deployment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');  //require express
 
-const PORT = 3001; //listen on this port 
 const reviews = require('./db/db.json');  //require the database where all the notes will be stored
 
 const app = express();
+
+var PORT = process.env.PORT || 3001;  //listen on this port 
 
 // Middleware for parsing application/json and urlencoded data
 app.use(express.json());
@@ -15,4 +16,4 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(3001, () => console.log('Express Server on port 3001!'));
+app.listen( PORT, () => console.log('Express Server on port 3001!'));
