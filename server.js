0173e7f5698a8b5a27975224d1c01cd04552857e
LaurenDarrimon <laurendarrimon@gmail.com>
2022-01-28 23:43:18 -0800

    added delete request api call to splice deleted note out of repsone arrya and re-write notes json file

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/notes', (req, res) =>
 app.get("/api/notes", function(req, res) {
 
     // Log our request to the terminal
-    console.info(`${req.method} request received to get reviews`);
+    console.info(`${req.method} request received to get notes`);
 
     //call the newly promisified readFromFile function, which will
     //return a promise of the json data in the file 
@@ -42,19 +42,15 @@ app.get("/api/notes", function(req, res) {
         console.log(JSON.parse(data));
         res.json(JSON.parse(data));
     });
-
 });
 
-
 // POST to receive a new note to save on the request body, 
-// then, add it to the db.json file
-//then return the new note to the client. 
+// then, add it to the db.json file & return the new note to the client. 
 
-
-// POST request to add a new note 
 app.post('/api/notes', (req, res) => {
     // Log that a POST request was received
-    console.info(`${req.method} request received to add a review`);
+    console.info(`${req.method} note request received`);
+    //log what the data to post is
     console.info(req.body);
   
     // Destructuring assignment for the items in the body of the post request 
@@ -84,11 +80,10 @@ app.post('/api/notes', (req, res) => {
                     if (error) {
                         console.error(error)
                     } else { 
-                        console.info('Successfully updated reviews!')
+                        console.info('Yay! We\'ve updated the notes!')
                     }
                 }
-            )
-            }
+            )}
         });
   
         const response = {
@@ -96,18 +91,64 @@ app.post('/api/notes', (req, res) => {
         body: newNote,
         };
   
-      console.log(response);
-      res.json(response);
+        console.log(response);
+        res.json(response);
     } else {
-      res.json('Error in posting note');
-    }
-  });
+            res.json('Error in posting note');
+        }
+});
+
+//DELETE /api/notes/:id should receive a query parameter 
+//that contains the id of a note to delete. To delete a note, 
+//you'll need to read all notes from the db.json file, 
+//remove the note with the given id property, 
+//and then rewrite the notes to the db.json file.
+
+app.delete("/api/notes/:id", (req, res) => {
+    //log that a delete request was recieved
+    console.info(`${req.method} request received to delete a note`);
+    //log that we are capturing the right data to delete. 
+    console.info(req.body);
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // take response string, parse into JSON object, store in new variable 
+            const parsedNotes = JSON.parse(data);
+
+            parsedNotes.forEach((note, index) => {
+                
+            //console.log(note.id)
+            //console.log(req.params.id)
+            //if the note id is equal to the id parameter passed in URL 
+            if (note.id === req.params.id){
+                // splice out that note from the array
+                parsedNotes.splice(index, 1);
+            }    
+            });
+
+            // write all the notes we're keeping back to the JSON file
+            fs.writeFile( './db/db.json', JSON.stringify(parsedNotes, null, 2),
+                (error) => {
+                    if (error) {
+                        console.error(error)
+                    } else { 
+                        console.info('The bad note was deleted.')
+                        //reload the notes page so it populates with fresh json data without the deleted note. 
+                        res.sendFile(path.join(__dirname, '/public/notes.html'));
+                    }
+                }
+            )
+        }
+    });
+}); 
 
 
 //WILDCARD route goes last so that it doesn't override other api routess, 
 //catches anything else. 
 app.get('/*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen( PORT, () => console.log('Express Server on port 3001!'));
