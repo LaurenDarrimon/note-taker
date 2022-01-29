933fef40b92573b4da980ce8b09d44bd0e9504b7
LaurenDarrimon <laurendarrimon@gmail.com>
2022-01-28 16:23:42 -0800

    add get routes for html files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');  //require express
+const path = require('path');
 
 const reviews = require('./db/db.json');  //require the database where all the notes will be stored
 
@@ -15,5 +16,16 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
 
 app.listen( PORT, () => console.log('Express Server on port 3001!'));
